bd049bee9b7cdef6c33cef393b733dc821b7eecb
Brett Jones <bhjones4545@gmail.com>
2021-07-20 20:18:45 -0600

    Add #finds_application method

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,9 @@
 class PetApplication < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  def self.finds_application(pet_id, application_id)
+    pet_apps = where("pet_id = #{pet_id} and application_id = #{application_id}")
+    pet_apps.first
+  end
 end
