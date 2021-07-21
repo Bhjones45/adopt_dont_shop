54582c20e2601823b052edb3de675006bbf0f0ac
Brett Jones <bhjones4545@gmail.com>
2021-07-20 20:25:09 -0600

    Add #finds_pets, refactor methods

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -3,7 +3,10 @@ class PetApplication < ApplicationRecord
   belongs_to :pet
 
   def self.finds_application(pet_id, application_id)
-    pet_apps = where("pet_id = #{pet_id} and application_id = #{application_id}")
-    pet_apps.first
+    pet_apps = where("pet_id = #{pet_id} and application_id = #{application_id}").first
+  end
+
+  def self.finds_pets(pet_id, application_id)
+    pet_apps = where("pet_id = #{pet_id} and application_id = #{application_id}").first.application.pets
   end
 end
