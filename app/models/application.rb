0335f95f3b388ac9ee93e56d976db3045aeb0692
Brett Jones <bhjones4545@gmail.com>
2021-07-20 22:40:46 -0600

    Create and add rejected method/test

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,4 +7,16 @@ class Application < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
+
+  def all_pets_approved?
+    pet_applications.all? do |application|
+      application.status == "Approved"
+    end
+  end
+
+  def pets_rejected?
+    pet_applications.any? do |application|
+      application.status == "Rejected"
+    end
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -25,5 +25,16 @@ RSpec.describe Application, type: :model do
 
       expect(application.all_pets_approved?).to eq(true)
     end
+
+    it '#pets_rejected?' do
+      shelter = Shelter.create!(name:'Boise Shelter', city: 'Boise', foster_program: false, rank: 7)
+      pet_1 = Pet.create!(adoptable: true, age: 2, breed: 'Bulldog', name: 'Butch', shelter_id: shelter.id)
+      pet_2 = Pet.create!(adoptable: true, age: 2, breed: 'German Shepard', name: 'Daisy', shelter_id: shelter.id)
+      application = Application.create!(name: "Taylor Hammond", street_address: "5234 Broadway Avenue", city: "Dallas", state: "Texas", zip_code: '86754', description: "I am very good person", status: "Pending")
+      pet_application = PetApplication.create!(pet: pet_1, application: application, status: "Approved")
+      pet_application = PetApplication.create!(pet: pet_2, application: application, status: "Rejected")
+
+      expect(application.pets_rejected?).to eq(true)
+    end
   end
 end
