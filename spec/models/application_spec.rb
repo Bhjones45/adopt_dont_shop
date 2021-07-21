ba06652817646a9c9843e46703142e9fed0369a4
Brett Jones <bhjones4545@gmail.com>
2021-07-20 22:34:46 -0600

    Create all pets approved test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:zip_code) }
   end
+
+  describe '#instance methods' do
+    it '#all_pets_approved?' do
+      shelter = Shelter.create!(name:'Boise Shelter', city: 'Boise', foster_program: false, rank: 7)
+      pet_1 = Pet.create!(adoptable: true, age: 2, breed: 'Bulldog', name: 'Butch', shelter_id: shelter.id)
+      pet_2 = Pet.create!(adoptable: true, age: 2, breed: 'German Shepard', name: 'Daisy', shelter_id: shelter.id)
+      application = Application.create!(name: "Taylor Hammond", street_address: "5234 Broadway Avenue", city: "Dallas", state: "Texas", zip_code: '86754', description: "I am very good person", status: "Pending")
+      pet_application = PetApplication.create!(pet: pet_1, application: application, status: "Approved")
+      pet_application = PetApplication.create!(pet: pet_2, application: application, status: "Approved")
+
+      expect(application.all_pets_approved?).to eq(true)
+    end
+  end
 end
